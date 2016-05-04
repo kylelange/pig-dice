89f40c23cf2dad4e7d36864b04a75ef305a94476
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-05-03 17:26:09 -0700

    create page layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,45 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+// game logic
+  // roll a die (6 sided?)
+  // if die = 1 it's the next player's turn
+  // if the die != 1 then the turn continues and the amount of die is added to
+  // turn score
+  // the player can "hold" and their turn score is added to their total score
+  // game is over when first player reaches 100 total score
+
+function Player() {
+  //isTurn?
+  //turnScore
+  //gameScore
+}
+
+function Die() {
+  //rolls die for each player/turn
+}
+
+function Game() {
+  //gameover?
+  //whose turn?
+  //player array
+}
+
+// UI logic
+// Setup Game: List Players
+// Turn - next player and roll until 1 or "hold"
+// Score - add turn score to total at end of turn
+// Check for winner, next player...
+var GameUI = {
+  //nextPlayer
+},
+{
+  //nextRoll
+},
+{
+  //display scores
+},
+{
+  //displayScore
+},
+{
+  //populateIdWithHTML
+},
